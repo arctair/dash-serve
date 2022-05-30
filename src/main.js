4c83c7417cda38d2367e8618fe5a8cb20f3fb0f4
tyler <tyler@cruftbusters.com>
2022-05-29 17:47:20 -0700

    serve mpd files

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
+const fs = require('fs')
 const http = require('http')
 const port = process.env.PORT || 8080
 
 function requestListener(request, response) {
   const urlComponents = request.url.split('/')
   const last = urlComponents[urlComponents.length - 1]
-  console.log(last)
-  response.write('hello world')
-  response.end()
+  if (last.endsWith('.mpd')) {
+    const manifestPath = `${process.env.DASH_DIR}/${last}`
+    fs.readFile(manifestPath, function (error, data) {
+      if (error) {
+        response.writeHead(500)
+        response.end(error.message)
+      } else {
+        response.writeHead(200)
+        response.end(data)
+      }
+    })
+  } else {
+    response.write('hello world')
+    response.end()
+  }
 }
 
 const server = http.createServer(requestListener)
