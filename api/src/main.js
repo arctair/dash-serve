8fd920a2edef6083165664c80cfb86037fc8eb00
tyler <tyler@cruftbusters.com>
2022-05-29 21:55:27 -0700

    api serves temporary manifest (mpd)

--- api/src/main.js
+++ api/src/main.js
@@ -8,12 +8,18 @@ function requestListener(request, response) {
   const urlComponents = request.url.split('/')
   const fileName = urlComponents[urlComponents.length - 1]
   const filePath = `${process.env.DASH_DIR}/${fileName}`
-  if (fileName.endsWith('.mpd')) {
-    copyFileToResponse(filePath, response)
-  } else if (fileName.endsWith('.m4s')) {
+  if (fileName.endsWith('.mpd') || fileName.endsWith('.m4s')) {
     const filePathTmp = `${filePath}.tmp`
     if (fs.existsSync(filePath)) {
-      copyFileToResponse(filePath, response)
+      fs.readFile(filePath, function (error, data) {
+        if (error) {
+          response.writeHead(500)
+          response.end(error.message)
+        } else {
+          response.writeHead(200)
+          response.end(data)
+        }
+      })
     } else if (fs.existsSync(filePathTmp)) {
       const tail = spawn('tail', ['-f', filePathTmp, '-n', '+1'])
       response.writeHead(200)
@@ -27,23 +33,14 @@ function requestListener(request, response) {
           clearInterval(interval)
         }
       })
+    } else {
+      response.writeHead(404)
+      response.end()
     }
   } else {
     response.writeHead(404)
     response.end()
   }
-}
-
-function copyFileToResponse(filePath, response) {
-  fs.readFile(filePath, function (error, data) {
-    if (error) {
-      response.writeHead(500)
-      response.end(error.message)
-    } else {
-      response.writeHead(200)
-      response.end(data)
-    }
-  })
 }
 
 const server = http.createServer(requestListener)
