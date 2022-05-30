7e192d29bcd63ac96f76cbdfd822912840fcb192
tyler <tyler@cruftbusters.com>
2022-05-29 19:32:28 -0700

    simple dashjs player

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,3 +1,38 @@
+import { useEffect, useRef } from 'react'
+import dashjs from 'dashjs'
+
+const manifestURL = 'https://snare.cc/dash/live.brunchyroll.mpd'
+const player = dashjs.MediaPlayer().create()
+player.updateSettings({
+  streaming: {
+    delay: {
+      liveDelay: 4,
+    },
+    liveCatchup: {
+      maxDrift: 0,
+      playbackRate: 0.5,
+      latencyThreshold: 60,
+    },
+  },
+})
+
 export default function App() {
-  return <>hello world</>
+  const ref = useRef()
+  useEffect(() => {
+    player.initialize(ref.current, manifestURL, true)
+  }, [])
+  return (
+    <video
+      ref={ref}
+      controls
+      style={{
+        maxWidth: '100%',
+        maxHeight: '100%',
+        position: 'absolute',
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    />
+  )
 }
