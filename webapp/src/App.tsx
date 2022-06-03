912d7b3702b528652d032d55f49b79c38438f205
tyler <tyler@cruftbusters.com>
2022-06-03 12:33:11 -0700

    extract usePauseControls

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,5 +1,6 @@
 import { MutableRefObject, useEffect, useRef, useState } from 'react'
 import dashjs from 'dashjs'
+import usePauseControls from './components/usePauseControls'
 
 const manifestURL = 'https://dash.snare.cc/live.brunchyroll.mpd'
 const player = dashjs.MediaPlayer().create()
@@ -19,7 +20,6 @@ player.updateSettings({
 export default function App() {
   const containerRef = useRef() as MutableRefObject<HTMLDivElement>
   const videoRef = useRef() as MutableRefObject<HTMLVideoElement>
-  const captivePause = useRef(false)
 
   const [videoSize, setVideoSize] = useState<{
     width?: number
@@ -56,33 +56,7 @@ export default function App() {
     return () => window.removeEventListener('resize', onResize)
   }, [])
 
-  useEffect(() => {
-    const onKeyDown = (event: KeyboardEvent) => {
-      const video = videoRef.current!
-      if (event.code === 'Space' && !video.paused) {
-        video.pause()
-        captivePause.current = true
-      }
-    }
-    document.addEventListener('keydown', onKeyDown)
-    return () => document.removeEventListener('keydown', onKeyDown)
-  }, [])
-
-  useEffect(() => {
-    const onKeyUp = (event: KeyboardEvent) => {
-      const video = videoRef.current!
-      if (
-        event.code === 'Space' &&
-        video.paused &&
-        !captivePause.current
-      ) {
-        video.play()
-      }
-      captivePause.current = false
-    }
-    document.addEventListener('keyup', onKeyUp)
-    return () => document.removeEventListener('keyup', onKeyUp)
-  }, [])
+  usePauseControls({ video: videoRef.current! })
 
   return (
     <div ref={containerRef} style={{ height: '100%', display: 'grid' }}>
