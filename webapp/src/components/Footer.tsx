365592f5b78176ef6bf2a06a560fce111f222b11
tyler <tyler@cruftbusters.com>
2022-06-03 14:07:18 -0700

    add conditional deployment link to status bar

--- webapp/src/components/Footer.tsx
+++ webapp/src/components/Footer.tsx
@@ -3,12 +3,37 @@ export default function Footer() {
     <div
       style={{
         color: '#CCC',
-        textAlign: 'right',
-        padding: '0.125rem 0.5rem',
+        display: 'flex',
+        justifyContent: 'flex-end',
         borderTop: '0.5px solid #CCC',
       }}
     >
-      {process.env.REACT_APP_GIT_VERSION}
+      <Block>
+        <a
+          href="https://snare.cc"
+          style={{
+            visibility:
+              process.env.REACT_APP_GIT_VERSION === 'dev'
+                ? 'visible'
+                : 'hidden',
+            textDecoration: 'none',
+            color: '#CCF',
+            fontWeight: 'bold',
+          }}
+        >
+          snare.cc
+        </a>
+      </Block>
+      <Block>{process.env.REACT_APP_GIT_VERSION}</Block>
+    </div>
+  )
+}
+
+type BlockProps = { children: React.ReactNode }
+function Block({ children }: BlockProps) {
+  return (
+    <div className="block" style={{ padding: '0 0.25rem' }}>
+      {children}
     </div>
   )
 }
